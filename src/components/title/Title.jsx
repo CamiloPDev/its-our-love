ad3aca333ff367cdae23f81fa89282f578e30a70
CamiloDev <cami20032017@gmail.com>
2024-12-03 16:58:49 -0500

    feat: component and test dimension

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Title.css'
+
+export default function Presentation (){
+
+
+    return (
+        <header className='presentation'>
+            <h1 className='title font'>happy 4th month, My Love</h1>
+            <h2 className='font'>i love you❤️</h2>
+        </header>
+    );
+}
